bbac267b0bde75697fc47d71962b4c856048eb8e
BartekDziurowicz <bartosz.dziurowicz@gmail.com>
2022-08-24 16:45:48 +0200

    3.30 Floating Point Precision challenge

--- src/academy/learnprogramming/Main.java
+++ src/academy/learnprogramming/Main.java
@@ -3,13 +3,9 @@ package academy.learnprogramming;
 public class Main {
 
     public static void main(String[] args) {
-        byte b = 1;
-        short s = 10;
-        int i = 100;
-        long ltotal = 50000l+10l*(b+s+i);
-        System.out.println("result "+ltotal);
-        short stotal = (short) (50000+10*(b+s+i));
-        System.out.println("result " +stotal);
+        double pounds = 12;
+        double kg = 12*0.4359237;
+        System.out.println(kg);
     }
 
 }
