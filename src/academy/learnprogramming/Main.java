0cf31ccf324b06cf9353f329c7cda312f9c12eb4
BartekDziurowicz <bartosz.dziurowicz@gmail.com>
2022-08-25 18:31:48 +0200

    3.40 Operator challenge

--- src/academy/learnprogramming/Main.java
+++ src/academy/learnprogramming/Main.java
@@ -3,9 +3,15 @@ package academy.learnprogramming;
 public class Main {
 
     public static void main(String[] args) {
-        double pounds = 12;
-        double kg = 12*0.4359237;
-        System.out.println(kg);
+        double first = 20.00;
+        double second = 80.00;
+        double total = (first + second) * 100;
+        double remainder = total%40.00;
+        boolean bool = (remainder==0) ? true : false;
+        System.out.println(bool);
+        if (!bool) {
+            System.out.println("Got some remainder!");
+        }
     }
 
 }
