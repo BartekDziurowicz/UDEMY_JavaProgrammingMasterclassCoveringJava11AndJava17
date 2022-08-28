f1528fae5b1a4e3658717a33f72db0e37ba99cc2
BartekDziurowicz <bartosz.dziurowicz@gmail.com>
2022-08-28 16:58:27 +0200

    4.49 Methods challenge

--- src/academy/learnprogramming/Main.java
+++ src/academy/learnprogramming/Main.java
@@ -2,31 +2,28 @@ package academy.learnprogramming;
 
 public class Main {
 
+    public static void displayHighScorePosition(String name, int score) {
+        System.out.println(name + " managed to get into position " + calculateHighScorePosition(score) + " on the high score table");
+    }
+
+    public static int calculateHighScorePosition(int playerScore) {
+        if (playerScore >= 1000) {
+            return 1;
+        } else if (playerScore >= 500) {
+            return 2;
+        } else if (playerScore >= 100) {
+            return 3;
+        } else {
+            return 4;
+        }
+    }
+
     public static void main(String[] args) {
 
-        boolean gameOver = true;
-        int score = 800;
-        int levelCompleted = 5;
-        int bonus = 100;
-
-        if (gameOver==true) {
-            int finalScore = score + (levelCompleted * bonus);
-            System.out.println(finalScore);
-
-            score = 10000;
-            levelCompleted = 8;
-            bonus = 200;
-            finalScore = score + (levelCompleted * bonus);
-            System.out.println(finalScore);
-        }
-
-        if (gameOver) {
-            score = 10000;
-            levelCompleted = 8;
-            bonus = 200;
-            int finalScore = score + (levelCompleted * bonus);
-            System.out.println(finalScore);
-        }
+        displayHighScorePosition("Bartek", 1500);
+        displayHighScorePosition("Bartek", 900);
+        displayHighScorePosition("Bartek", 400);
+        displayHighScorePosition("Bartek", 50);
 
     }
 
