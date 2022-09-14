177d389f6ddf61ef66c936e75e346254c16d2b75
BartekDziurowicz <bartosz.dziurowicz@gmail.com>
2022-09-14 11:11:50 +0200

    Exercise 44: Mobile Phone

--- src/academy/learnprogramming/Main.java
+++ src/academy/learnprogramming/Main.java
@@ -6,10 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int[] array = {1,2,3,4,5};
-        ReverseArray reverseArray = new ReverseArray();
-        reverseArray.reverse(array);
-        System.out.println(Arrays.toString(array));
+
 
     }
 }
